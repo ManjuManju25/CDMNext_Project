5c7717f4b36df970f4378c1ecac87152100066ec
Nadiya Maduru <nadiya.maduru@shravas.com>
2018-07-26 11:07:19 +0530

    adding runnerfile as search.feature

--- src/test/java/CDMNext/runner/TestRunner.java
+++ src/test/java/CDMNext/runner/TestRunner.java
@@ -1,20 +1,22 @@
 package CDMNext.runner;
 
 import cucumber.api.CucumberOptions;
-
 import cucumber.api.testng.TestNGCucumberRunner;
 import cucumber.api.testng.CucumberFeatureWrapper;
 import org.testng.annotations.*;
 import CDMNext.StepDefinations.login;
 import CDMNext.util.SendmailWithAttachment;
 
-@CucumberOptions(features = "src/test/java/CDMNext/Features/filters.feature", glue = {
-		"CDMNext.StepDefinations" }, tags = {
-				"@KeywordSearch,@UnitFilter,@SourceFilter,@DateFilter,@FrequencyFilter,@SeriesIDSearch,@SeriesTagSearch" }, dryRun = false, plugin = {
-						"pretty", "html:target/cucumber-reports/cucumber-pretty",
-						"json:target/cucumber-reports/CucumberTestReport.json",
-						"com.cucumber.listener.ExtentCucumberFormatter:target/report.html",
-						"rerun:target/cucumber-reports/rerun.txt" })
+@CucumberOptions(features = "src/test/java/CDMNext/Features/search.Feature", glue = {
+		"CDMNext.StepDefinations" }, tags = { "@KeywordSearch,@ChineseKeywordSearch" },
+		// tags =
+
+		// {"@KeywordSearch,@UnitFilter,@SourceFilter,@DateFilter,@FrequencyFilter,@SeriesIDSearch,@SeriesTagSearch"
+		// },
+		dryRun = false, plugin = { "pretty", "html:target/cucumber-reports/cucumber-pretty",
+				"json:target/cucumber-reports/CucumberTestReport.json",
+				"com.cucumber.listener.ExtentCucumberFormatter:target/report.html",
+				"rerun:target/cucumber-reports/rerun.txt" })
 public class TestRunner {
 
 	private TestNGCucumberRunner testNGCucumberRunner;
@@ -51,8 +53,8 @@ public class TestRunner {
 			login.driver.quit();
 			login.driver = null;
 		}
-		 login.Log4j.info("\n ****Inside Email*****");
-		 SendmailWithAttachment.report();
+		login.Log4j.info("\n ****Inside Email*****");
+		SendmailWithAttachment.report();
 	}
 
 }
