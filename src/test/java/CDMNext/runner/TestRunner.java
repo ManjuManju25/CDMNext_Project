ed2d266d9d9f7ca03dcdd57ec28198afa8f86ec1
NadiyaMaduru <nmaduru@shravas.com>
2019-03-29 12:47:27 +0530

    Executing DB cases

--- src/test/java/CDMNext/runner/TestRunner.java
+++ src/test/java/CDMNext/runner/TestRunner.java
@@ -13,7 +13,7 @@ import CDMNext.StepDefinations.login;
 		strict = true,
 	  	features = "src/test/java/CDMNext/Features/",
 		glue = { "CDMNext.StepDefinations" }, 
-	     tags = {"@DB,@UI,@ExelDatacomparision,@FilterSearch" },
+	     tags = {"@DB" },
 		//tags = {"@DB,@UI,@ExelDatacomparision,@FilterSearch,@Search,@MySeries,@SeriesTab,@Footnote" },
 		monochrome = true,
 		//dryRun = true,
