1b92560f0b975ca9b2ca8e83a328563c01ff9209
NadiyaMaduru <nmaduru@shravas.com>
2020-09-10 19:38:53 +0530

    changed runner file

--- src/test/java/CDMNext/runner/TestRunner.java
+++ src/test/java/CDMNext/runner/TestRunner.java
@@ -12,7 +12,7 @@ import CDMNext.StepDefinations.login;
 @CucumberOptions(// features="classpath:",
 		strict = true, features = "src/test/java/CDMNext/Features/",
 		glue = { "CDMNext.StepDefinations" }, 
-		tags = {"@Attachments"}, 
+		tags = {"@FilterSearch1"}, 
 		monochrome = true,
 		//dryRun = true,
 		plugin = {"html:target/cucumber-reports/cucumber-pretty",
